1db5f88c8c471221a9d35fb0167819ebbe6e2f79
Matthew Ruffalo <matthew.ruffalo@case.edu>
2013-03-20 12:30:38 -0400

    smoker.c: more formatting tweaks that I missed the first time

--- smoker.c
+++ smoker.c
@@ -5,14 +5,14 @@ int main(int argc, char *argv[])
 	int semid, shmid;
 	struct common *shared;
 
-	int mypid=getpid();
-	int smokernum=atoi(argv[1]);
+	int mypid = getpid();
+	int smokernum = atoi(argv[1]);
 
-	semid=semget(SEMKEY,NUM_SEMS,0777);
-	shmid=shmget(SHMKEY,1*K,0777);
-	shared=(struct common *)shmat(shmid,0,0);
+	semid = semget(SEMKEY, NUM_SEMS, 0777);
+	shmid = shmget(SHMKEY, 1*K, 0777);
+	shared = (struct common *)shmat(shmid, 0,0);
 
-    while (1) {
+	while (1) {
 		P(semid, SEM_SMOKER);
 		if ((shared->item1 != smokernum) && (shared->item2 != smokernum)) {
 			printf("SMOKER %d (pid %d) smokes...\n", smokernum, mypid);
